b7a3142fc725e2038c6d5b188bdc14b8fcd12639
ampcpmgp <ampcpmgp@gmail.com>
2016-04-14 01:06:44 +0900

    引数ごとに段落とし

--- $.linkup.js
+++ $.linkup.js
@@ -9,7 +9,14 @@
 				var i;
 				for (i = 0; i < obj.task.length; ++i) {
 					if (eval('typeof (' + obj.task[i].handler + ')') === 'function') {
-						$(obj.el).on(obj.task[i].event, obj.task[i].selector, $.proxy(eval(obj.task[i].handler), $(obj.el)));
+						$(obj.el).on(
+							obj.task[i].event,
+							obj.task[i].selector,
+						 	$.proxy(
+								eval(obj.task[i].handler),
+						  		$(obj.el)
+							)
+						);
 						$(obj.task[i].selector).trigger('ready');
 					}
 				}

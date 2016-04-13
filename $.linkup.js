49e6df6cfe3f9324d8ce621a5a5e96b242147a05
ampcpmgp <ampcpmgp@gmail.com>
2016-04-14 01:34:09 +0900

    共通で使えるため変数メソッドを書いておく

--- $.linkup.js
+++ $.linkup.js
@@ -3,12 +3,15 @@
 /*jslint evil: true, plusplus: true*/
 (function ($) {
 	'use strict';
+	var checkStrIsFunction = function(str) {
+		return eval('typeof ' + str) === 'function';
+	}
 	$.extend({
 		linkup: function (obj) {
 			$(document).ready(function () {
 				var i;
 				for (i = 0; i < obj.task.length; ++i) {
-					if (eval('typeof (' + obj.task[i].handler + ')') === 'function') {
+					if (checkStrIsFunction(obj.task[i].handler)) {
 						var evalStr =
 							'(function(e){'+
 							obj.task[i].handler+

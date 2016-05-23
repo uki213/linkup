e57562acef259136bcfea615edba7a769c190cf3
Niimi Youki <niimiyouki@gmail.com>
2016-05-23 13:58:46 +0900

    evalがいると思ったのは気の迷いだった。

--- $.linkup.js
+++ $.linkup.js
@@ -1,6 +1,5 @@
 // $.linkup.js (https://github.com/uki213/linkup)
 /*global jQuery */
-/*jslint evil: true*/
 (function ($) {
 	'use strict';
 	$.extend({
@@ -9,7 +8,7 @@
 				var i;
 				for (i = 0; i < obj.task.length; i = i + 1) {
 					if (typeof obj.task[i].handler === 'function') {
-						$(obj.el).on(obj.task[i].event, obj.task[i].selector, $.proxy(eval(obj.task[i].handler), $(obj.el)));
+						$(obj.el).on(obj.task[i].event, obj.task[i].selector, $.proxy(obj.task[i].handler, $(obj.el)));
 						$(obj.task[i].selector).trigger('ready');
 					}
 				}

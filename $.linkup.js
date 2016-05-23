e979ff3e4cbbec1ed6f64c090ff89bdc1f83cd42
Niimi Youki <niimiyouki@gmail.com>
2016-05-23 13:41:56 +0900

    やっぱevalあったほうがいいかも

--- $.linkup.js
+++ $.linkup.js
@@ -8,7 +8,7 @@
 				var i;
 				for (i = 0; i < obj.task.length; i = i + 1) {
 					if (typeof obj.task[i].handler === 'function') {
-						$(obj.el).on(obj.task[i].event, obj.task[i].selector, $.proxy(obj.task[i].handler, $(obj.el)));
+						$(obj.el).on(obj.task[i].event, obj.task[i].selector, $.proxy(eval(obj.task[i].handler), $(obj.el)));
 						$(obj.task[i].selector).trigger('ready');
 					}
 				}
